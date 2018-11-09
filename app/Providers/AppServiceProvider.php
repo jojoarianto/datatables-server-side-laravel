b5fc0ab132ac97d03b254315d86e05f09bee18df
Joko Irianto <irianto94@outlook.com>
2018-11-09 11:46:59 +0700

    fix https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -14,7 +15,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         if (env('REDIRECT_HTTPS') == 'true') {
-            \URL::forceSchema('https');
+            URL::forceSchema('https');
         }
     }
 

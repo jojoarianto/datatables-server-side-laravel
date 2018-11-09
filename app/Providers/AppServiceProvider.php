1567b08a5e1617242daf8d71dd810cc887a7c020
Joko Irianto <irianto94@outlook.com>
2018-11-09 11:43:39 +0700

    fix https ;

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,7 +13,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        if (env('REDIRECT_HTTPS') == 'true') {
+            \URL::forceSchema('https');
+        }
     }
 
     /**
